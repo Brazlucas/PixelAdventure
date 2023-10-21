e1b483eb1d6d8b4fbaed3250333778ec79b19287
Lucas Braz <lukkascomics@gmail.com>
2023-10-21 16:13:39 -0300

    created enemy2

--- Enemy2.cs
+++ Enemy2.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class Enemy2 : MonoBehaviour
 {
     private Rigidbody2D rig;
+
     private Animator anim;
+
     public float speed;
+
     public Transform rightCol;
+
     public Transform leftCol;
+
     public Transform headPoint;
-    private bool colliding; 
-    
+
+    private bool colliding;
+
+    public LayerMask layer;
+
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
@@ -21,6 +29,13 @@ public class Enemy2 : MonoBehaviour
     void Update()
     {
         rig.velocity = new Vector2(speed, rig.velocity.y);
-        Debug.Log(speed);
+
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position);
+
+        if (colliding)
+        {
+            transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
+            speed *= -1f;
+        }
     }
 }
